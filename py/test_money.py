fc2dc8e58b722fd3921a9039cd3f8f2dffc6fdc9
JJL <jialiang8455@gmail.com>
2023-01-11 15:16:27 +0800

    feat: addition feature for Moneys in the same currency done

--- py/test_money.py
+++ py/test_money.py
@@ -1,13 +1,15 @@
 import unittest
 from typing import Union
 from decimal import Decimal
+import functools
+import operator
 
 
 class TestMoney(unittest.TestCase):
     def testMultiplicationInDollars(self, ):
         fiveDollars = Money(5, "USD")
         tenDollars = Money(10, "USD")
-        self.assertEqual(fiveDollars, tenDollars)
+        self.assertEqual(tenDollars, fiveDollars.times(2))
 
     def testMultiplicationInEuros(self, ):
         tenEuros = Money(10, "EUR")
@@ -21,6 +23,16 @@ class TestMoney(unittest.TestCase):
         expectedMoneyAfterDivision = Money(1000.5, "KRW")
         self.assertEqual(expectedMoneyAfterDivision.amount, actualMoneyAfterDivision.amount)
         self.assertEqual(expectedMoneyAfterDivision.currency, actualMoneyAfterDivision.currency)
+
+    def testAddition(self, ):
+        fiveDollars = Money(5, "USD")
+        tenDollars = Money(10, "USD")
+        fifteenDollars = Money(15, "USD")
+        portfolio = Portfolio()
+        portfolio.add(fiveDollars, tenDollars)
+        self.assertEqual(fifteenDollars, portfolio.evaluate("USD"))
+
+
 class Money:
     def __init__(self, amount: Union[int, float, Decimal], currency: str):
         self.amount = amount
@@ -35,6 +47,20 @@ class Money:
 
     def __eq__(self, otherInstance: object) -> bool:
         return self.amount == otherInstance.amount and self.currency == otherInstance.currency
+
+class Portfolio:
+    def __init__(self, ):
+        self.moneys = []
+        return
+
+    def add(self, *moneys):
+        # self.moneys = [*self.moneys, *moneys]
+        self.moneys.extend(moneys)
+        return
+
+    def evaluate(self, currency: str):
+        total = functools.reduce(operator.add, map(lambda money: money.amount, self.moneys), 0)
+        return Money(total, currency)
 
 if __name__ == "__main__":
     unittest.main()
