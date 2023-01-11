300f23a85313f3b4db4094cbdb4598443b0b9069
JJL <jialiang8455@gmail.com>
2023-01-11 14:04:32 +0800

    feat: add currency test

--- py/test_money.py
+++ py/test_money.py
@@ -1,4 +1,19 @@
 import unittest
+from typing import Union
+from decimal import Decimal
+
+
+class TestMoney(unittest.TestCase):
+    def testMultiplication(self, ):
+        fiver = Dollar(5)
+        tenner = fiver.times(2)
+        self.assertEqual(10, tenner.amount)
+
+    def testMultiplicationInEuros(self, ):
+        tenEuros = Money(10, "EUR")
+        twentyEuros = tenEuros.times(2)
+        self.assertEqual(20, twentyEuros.amount)
+        self.assertEqual("EUR", tenEuros.currency)
 
 class Dollar:
     def __init__(self, amount):
@@ -8,11 +23,15 @@ class Dollar:
     def times(self, multiplier):
         return Dollar(self.amount * multiplier)
 
-class TestMoney(unittest.TestCase):
-    def testMultiplication(self, ):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEqual(10, tenner.amount)
+class Money:
+    def __init__(self, amount: Union[int, float, Decimal], currency: str):
+        self.amount = amount
+        self.currency = currency
+        return
+
+    def times(self, multiplier: Union[int, float]):
+        return Money(self.amount * multiplier, self.currency)
+
 
 if __name__ == "__main__":
     unittest.main()
